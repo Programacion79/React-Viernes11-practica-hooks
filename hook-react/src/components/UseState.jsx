189ba7a24756711c530ea0dcdd431a7d7846a4f9
sebastian ramirez jaramillo <sramirezja@cesde.net>
2025-04-11 20:34:24 -0500

    Se crea el componente Cambiar Color

--- hook-react/src/components/UseState.jsx
+++ hook-react/src/components/UseState.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function UseState() {
   const [contador, setContador] = useState(0);
+  const [mostrar, setMostrar] = useState(false);
+  useEffect(()=>{
+        console.log("componente renderizado");
+  });
+  useEffect(()=>{
+    console.log("componente renderizado 1 vez");
+},[]);
+useEffect(()=>{
+    console.log("contador  o contenido actualizado");
+    console.log("contenido actualizado");
+    document.title =`Contador ${contador}`;
+
+},[contador, mostrar]);
 
   return (
     <>
-      <div>
-        Contador: {contador}
-      </div>
-      <button
-        onClick={() => setContador(contador + 1)}
-      >
-        Aumentar
+      {mostrar ? (
+        <>
+          <div>Contador: {contador}</div>
+
+          <button
+            onClick={() => {
+              setContador(contador + 1);
+              console.log(contador); // Nota: este valor se ve "atrasado" por cómo funciona React
+            }}
+          >
+            Aumentar
+          </button>
+        </>
+      ) : (
+        ""
+      )}
+      
+      <br />
+      
+      <button onClick={() => setMostrar(!mostrar)}>
+        Mostrar / Aumentar
       </button>
     </>
   );
